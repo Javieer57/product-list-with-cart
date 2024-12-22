cd9f3c3816509f14696b6791c940861249dc1ae1
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2024-12-22 15:44:11 -0600

    feat: add desserts list to main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { DessertsGrid } from "./components/DessertsGrid";
+
 function App() {
   return (
     <main className="grid gap-8 px-6 py-5 md:grid-cols-3">
-      <section className="md:col-span-2">
-        <h2 className="font-bold text-4xl">Desserts</h2>
+      <section className="md:col-span-2 space-y-7">
+        <h2 className="font-bold text-4xl text-rose-900">Desserts</h2>
+        <DessertsGrid />
       </section>
       <section className="md:col-span-1">
         <h2 className="font-bold text-2xl">Your Cart</h2>
--- src/components/DessertsGrid.tsx
+++ src/components/DessertsGrid.tsx
@@ -3,7 +3,7 @@ import { DessertCard } from "./DessertCard";
 
 export const DessertsGrid = () => {
   return (
-    <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-8">
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-8">
       {data.map((dessert) => (
         <DessertCard data={dessert} key={dessert.name} />
       ))}
