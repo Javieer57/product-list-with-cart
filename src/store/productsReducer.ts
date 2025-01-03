10a21934d9b147feea88d4c240ae21ae008660d1
Javier Eufracio <65838690+Javieer57@users.noreply.github.com>
2025-01-02 20:45:17 -0600

    feat: add products reducer

---
+++ src/store/productsReducer.ts
@@ -0,0 +1,61 @@
+type Product = { id: number; name: string; quantity: number };
+export type State = {
+  selectedProducts: Record<number, Product>;
+};
+export type Action =
+  | { type: "ADD_PRODUCT"; product: Product }
+  | { type: "REMOVE_PRODUCT"; productId: number };
+
+export const initialState: State = { selectedProducts: [] };
+
+export function productsReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      const existingItem = state.selectedProducts[action.product.id];
+
+      return {
+        ...state,
+        selectedProducts: {
+          ...state.selectedProducts,
+          [action.product.id]: {
+            ...action.product,
+            quantity: existingItem ? action.product.quantity + 1 : 1,
+          },
+        },
+      };
+    // return {
+    //   ...state,
+    //   selectedProducts: {
+    //     ...state.selectedProducts,
+    //     [action.product.id]: existingItem
+    //       ? { ...existingItem, quantity: existingItem.quantity + 1 }
+    //       : { ...action.product, quantity: 1 },
+    //   },
+    // };
+    case "REMOVE_PRODUCT":
+      const { [action.productId]: removedItem, ...rest } =
+        state.selectedProducts;
+
+      if (removedItem.quantity > 1) {
+        return {
+          ...state,
+          selectedProducts: {
+            ...rest,
+            [action.productId]: {
+              ...removedItem,
+              quantity: removedItem.quantity - 1,
+            },
+          },
+        };
+      }
+
+      return {
+        ...state,
+        selectedProducts: {
+          ...rest,
+        },
+      };
+    default:
+      throw new Error("Acción no reconocida");
+  }
+}
